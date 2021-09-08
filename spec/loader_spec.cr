8a007c48c1bc765fffcc055c85679a197065316d
Caspian Baska <email@caspian.computer>
2021-09-08 13:45:45 +1000

    test: add a test for expected behaviour

--- spec/loader_spec.cr
+++ spec/loader_spec.cr
@@ -7,6 +7,7 @@ module PlaceOS::FrontendLoader
 
     Spec.before_each do
       repository = example_repository
+      expected_path = File.join(TEST_DIR, repository.folder_name)
       reset
     end
 
@@ -95,6 +96,34 @@ module PlaceOS::FrontendLoader
     end
 
     describe "branches" do
+      it "is unaffected by queries" do
+        checked_out_commit = "f7c6d8fb810c2be78722249e06bbfbda3d30d355"
+        branch = "test-fixture"
+        folder = UUID.random.to_s
+        repo = PlaceOS::Model::Generator.repository(type: :interface).tap do |r|
+          r.name = "compiler"
+          r.uri = "https://github.com/placeos/compiler"
+          r.username = "robot@place.tech"
+          r.branch = branch
+          r.commit_hash = checked_out_commit
+          r.folder_name = folder
+        end
+        loader = Loader.new
+
+        loader.process_resource(:created, repo).success?.should be_true
+
+        expected_path = File.join(loader.content_directory, folder)
+
+        Dir.exists?(expected_path).should be_true
+        Compiler::Git.current_branch(expected_path).should eq branch
+
+        Api::Repositories.branches(folder, loader)
+        Api::Repositories.commits(folder, branch, loader: loader).not_nil!.should_not be_empty
+        Api::Repositories.commits(folder, "master", loader: loader).not_nil!.should_not be_empty
+
+        Compiler::Git.current_branch(expected_path).should eq branch
+      end
+
       it "loads a specific branch" do
         loader = Loader.new
 
@@ -103,8 +132,6 @@ module PlaceOS::FrontendLoader
 
         loader.process_resource(:created, repository).success?.should be_true
         Dir.exists?(expected_path).should be_true
-
-        Compiler::Git.current_branch(expected_path).should eq branch
       end
 
       it "switches branches" do
