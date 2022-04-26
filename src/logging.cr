4b817642cca8ff4fb951ae09582292a8f62dacdd
Stephen von Takach <steve@place.tech>
2022-04-26 17:58:06 +1000

    feat(logging): optional log level from ENV var

--- src/logging.cr
+++ src/logging.cr
@@ -5,5 +5,5 @@ require "./constants"
 module PlaceOS::FrontendLoader::Logging
   ::Log.progname = APP_NAME
   log_level = PlaceOS::FrontendLoader.production? ? Log::Severity::Info : Log::Severity::Debug
-  ::Log.setup "*", log_level, PlaceOS::LogBackend.log_backend
+  ::Log.setup_from_env default_sources: "*", default_level: log_level, backend: PlaceOS::LogBackend.log_backend, log_level_env: "LOG_LEVEL"
 end
