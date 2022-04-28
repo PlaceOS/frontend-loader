87675d00e27ecb39c852cec356bdf236bdbc3230
Caspian Baska <email@caspian.computer>
2022-04-28 15:33:47 +1000

    fix(telemetry): seperate telemetry file

--- src/logging.cr
+++ src/logging.cr
@@ -1,5 +1,4 @@
 require "placeos-log-backend"
-require "placeos-log-backend/telemetry"
 
 require "./constants"
 
@@ -7,9 +6,4 @@ module PlaceOS::FrontendLoader::Logging
   ::Log.progname = APP_NAME
   log_level = PlaceOS::FrontendLoader.production? ? Log::Severity::Info : Log::Severity::Debug
   ::Log.setup_from_env default_sources: "*", default_level: log_level, backend: PlaceOS::LogBackend.log_backend, log_level_env: "LOG_LEVEL"
-
-  PlaceOS::LogBackend.configure_opentelemetry(
-    service_name: APP_NAME,
-    service_version: VERSION,
-  )
 end
