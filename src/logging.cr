95260aab3b35e70c526fc93e038006a797f6f8b1
Caspian Baska <email@caspian.computer>
2022-05-03 17:33:01 +1000

    fix(telemetry): ensure `Instrument` in scope

--- src/logging.cr
+++ src/logging.cr
@@ -18,6 +18,7 @@ module PlaceOS::FrontendLoader::Logging
 
   builder = ::Log.builder
   builder.bind("*", log_level, log_backend)
+
   namespaces.each do |namespace|
     builder.bind(namespace, log_level, log_backend)
 
@@ -29,7 +30,8 @@ module PlaceOS::FrontendLoader::Logging
   ::Log.setup_from_env(
     default_level: log_level,
     builder: builder,
-    backend: log_backend
+    backend: log_backend,
+    log_level_env: "LOG_LEVEL",
   )
 
   # Configure Sentry
