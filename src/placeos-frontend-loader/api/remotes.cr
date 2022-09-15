3d44cafd0aa4ba7abd8b4d82a69b4ab09a1288c8
Stephen von Takach <steve@place.technology>
2022-09-15 18:05:56 +1000

    feat(api/remotes): allow querying protected remotes

--- src/placeos-frontend-loader/api/remotes.cr
+++ src/placeos-frontend-loader/api/remotes.cr
@@ -7,11 +7,20 @@ module PlaceOS::FrontendLoader::Api
     # =====================
 
     @[AC::Route::Filter(:before_action)]
-    protected def get_repository_url(repository_url : String)
+    protected def get_repository_url(
+      @[AC::Param::Info(description: "the git url that represents the repository", example: "https://github.com/PlaceOS/PlaceOS.git")]
+      repository_url : String,
+      @[AC::Param::Info(description: "a username for access if required", example: "steve")]
+      username : String? = nil,
+      @[AC::Param::Info(description: "the password or access token as required", example: "ab34cfe4567")]
+      password : String? = nil,
+    )
       @repository_url = repository_url
     end
 
     getter! repository_url : String
+    getter username : String? = nil
+    getter password : String? = nil
 
     # =====================
     # Routes
@@ -20,7 +29,7 @@ module PlaceOS::FrontendLoader::Api
     # Returns an array of releases for a repository
     @[AC::Route::GET("/:repository_url/releases")]
     def releases : Array(String)
-      repo = GitRepository.new(repository_url)
+      repo = GitRepository.new(repository_url, username, password)
       if repo.is_a?(GitRepository::Releases)
         repo.releases
       else
@@ -38,7 +47,7 @@ module PlaceOS::FrontendLoader::Api
       @[AC::Param::Info(description: "the file we want to grab commits from", example: "src/place/meet.cr")]
       file : String? = nil
     ) : Array(GitRepository::Commit)
-      repo = GitRepository.new(repository_url)
+      repo = GitRepository.new(repository_url, username, password)
       branch = branch || repo.default_branch
       file ? repo.commits(branch, file, depth) : repo.commits(branch, depth)
     end
@@ -46,14 +55,14 @@ module PlaceOS::FrontendLoader::Api
     # Returns an array of branches
     @[AC::Route::GET("/:repository_url/branches")]
     def branches : Array(String)
-      repo = GitRepository.new(repository_url)
+      repo = GitRepository.new(repository_url, username, password)
       repo.branches.keys
     end
 
     # Returns an array of tags
     @[AC::Route::GET("/:repository_url/tags")]
     def tags : Array(String)
-      repo = GitRepository.new(repository_url)
+      repo = GitRepository.new(repository_url, username, password)
       repo.tags.keys
     end
   end
