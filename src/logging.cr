68712a3b4d42d29ea7a77a8f1c8e681b7d072c52
Ali Naqvi <syed.alinaqvi@gmail.com>
2024-07-24 10:55:09 +0800

    refactor: PPT-1456 Remove Raven dependency

--- src/logging.cr
+++ src/logging.cr
@@ -1,15 +1,9 @@
 require "placeos-log-backend"
 
-require "raven"
-require "raven/integrations/action-controller"
-
 require "./constants"
 
 module PlaceOS::FrontendLoader::Logging
   ::Log.progname = APP_NAME
-
-  standard_sentry = Raven::LogBackend.new
-  comprehensive_sentry = Raven::LogBackend.new(capture_all: true)
 
   # Logging configuration
   log_backend = PlaceOS::LogBackend.log_backend
@@ -21,10 +15,6 @@ module PlaceOS::FrontendLoader::Logging
 
   namespaces.each do |namespace|
     builder.bind(namespace, log_level, log_backend)
-
-    # Bind raven's backend
-    builder.bind namespace, :info, standard_sentry
-    builder.bind namespace, :warn, comprehensive_sentry
   end
 
   ::Log.setup_from_env(
@@ -33,9 +23,6 @@ module PlaceOS::FrontendLoader::Logging
     backend: log_backend,
     log_level_env: "LOG_LEVEL",
   )
-
-  # Configure Sentry
-  Raven.configure &.async=(true)
 
   PlaceOS::LogBackend.register_severity_switch_signals(
     production: FrontendLoader.production?,
